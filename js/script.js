def513b30dd2943d6c96d039a8faf7279c3844e8
Ramiro Gómez <code@ramiro.org>
2017-02-14 02:02:07 +0100

    new deployment

---
+++ js/script.js
@@ -0,0 +1,3 @@
+Array.from(document.getElementsByClassName('e')).forEach(function(elt) {
+    elt.innerHTML = '<a href="mailto:' + elt.title + '@guitarstreams.com">' + elt.textContent + '</a>';
+});
