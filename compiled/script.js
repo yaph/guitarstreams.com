73305e8035f6bf6ef88f86f93da67bb829f09f7b
Ramiro Gómez <code@ramiro.org>
2018-10-05 02:10:50 +0200

    new deployment

--- compiled/script.js
+++ compiled/script.js
@@ -1,1 +1,1 @@
-'use strict';var bLazy=new Blazy;Array.from(document.getElementsByClassName('e')).forEach(function(elt){elt.innerHTML='<a href="mailto:'+elt.title+'@guitarstreams.com">'+elt.textContent+'</a>'});
+"use strict";var bLazy=new Blazy;
